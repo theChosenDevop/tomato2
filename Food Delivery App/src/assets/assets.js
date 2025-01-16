a2dc57c48d1d6298461ad885a4919cb44eda4fdd
theChosenDevop <tobbey.adesanya@gmail.com>
2025-01-16 14:54:53 -0800

    new update

---
+++ Food Delivery App/src/assets/assets.js
@@ -0,0 +1,410 @@
+import basket_icon from "./basket.png";
+import logo from "./Tomato.png";
+import search_icon from "./search.png";
+
+import menu_1 from "./menu_1-CpSfC1Ff.png";
+import menu_2 from "./menu_2-6QL_uDtg.png";
+import menu_3 from "./menu_3-2xw_iDUH.png";
+import menu_4 from "./menu_4-CpXAwO71.png";
+import menu_5 from "./menu_5-BLqPAi9S.png";
+import menu_6 from "./menu_6-BAKCTvIj.png";
+import menu_7 from "./menu_7-Dbn_MJmR.png";
+import menu_8 from "./menu_8-D3TIbU8x.png";
+
+import food_1 from "./food_1-iEOlQHK4.png";
+import food_2 from "./food_2-Bviin0XJ.png";
+import food_3 from "./food_3-DFGIQWuA.png";
+import food_4 from "./food_4-DA8g0Kmx.png";
+import food_5 from "./food_5-DCZk6AQV.png";
+import food_6 from "./food_6-Cz8hlc5i.png";
+import food_7 from "./food_7-xL5P3oOv.png";
+import food_8 from "./food_8-Cl7of1B4.png";
+import food_9 from "./food_9-CQtsjFOM.png";
+import food_10 from "./food_10-Cdj7aC_-.png";
+import food_11 from "./food_11-DfRPpaFP.png";
+import food_12 from "./food_12-TcaAZGUd.png";
+import food_13 from "./food_13-k_d-dzFx.png";
+import food_14 from "./food_14-C9vU_xdJ.png";
+import food_15 from "./food_15-CLR4VtEo.png";
+import food_16 from "./food_16-DqWMQk4h.png";
+import food_17 from "./food_17-B8PBV8Ml.png";
+import food_18 from "./food_18-D9lmXWyv.png";
+import food_19 from "./food_19-CFHillXF.png";
+import food_20 from "./food_20-CTe0fZJ7.png";
+import food_21 from "./food_21-BrJc8UvX.png";
+import food_22 from "./food_22-BVMfdZ1K.png";
+import food_23 from "./food_23-BxkRb3Vg.png";
+import food_24 from "./food_24-D5JYPl2r.png";
+import food_25 from "./food_25-BzfbEGNp.png";
+import food_26 from "./food_26-ButgZpoJ.png";
+import food_27 from "./food_27-Bd1jqUGP.png";
+import food_28 from "./food_28-D2lOoPiC.png";
+import food_29 from "./food_29-B8GW2frB.png";
+import food_30 from "./food_30-CfTld12c.png";
+import food_31 from "./food_31-BgWMzztU.png";
+import food_32 from "./food_32-BKt-jRyi.png";
+
+import header_img from "./header_img.png";
+import rating_starts from "./rating.png";
+import add_icon_green from "./plus.png";
+import add_icon_white from "./nontransparentplus.png";
+import remove_icon_red from "./delete.png";
+import app_store from "./app_store-C8O_cY6s.png";
+import play_store from "./play_store-B2tFv0Hy.png";
+import linkedin_icon from "./linkedin.png";
+import twitter_icon from "./x.png";
+// import facebook_icon from './'
+// import cross_icon from './'
+// import selector_icon from './'
+// import profile_icon from './'
+// import logout_icon from './'
+// import bag_icon from './'
+// import parcel_icon from './'
+
+export const assets = {
+  logo,
+  basket_icon,
+  header_img,
+  search_icon,
+  rating_starts,
+  add_icon_green,
+  add_icon_white,
+  remove_icon_red,
+  app_store,
+  play_store,
+  linkedin_icon,
+  twitter_icon,
+  // facebook_icon,
+  // cross_icon,
+  // selector_icon,
+  // profile_icon,
+  // logout_icon,
+  // bag_icon,
+  // parcel_icon
+};
+
+export const menu_list = [
+  {
+    menu_name: "salad",
+    menu_image: menu_1,
+  },
+  {
+    menu_name: "Rolls",
+    menu_image: menu_2,
+  },
+  {
+    menu_name: "Deserts",
+    menu_image: menu_3,
+  },
+  {
+    menu_name: "Sandwich",
+    menu_image: menu_4,
+  },
+  {
+    menu_name: "Cake",
+    menu_image: menu_5,
+  },
+  {
+    menu_name: "Pure Veg",
+    menu_image: menu_6,
+  },
+  {
+    menu_name: "Pasta",
+    menu_image: menu_7,
+  },
+  {
+    menu_name: "Noodles",
+    menu_image: menu_8,
+  },
+];
+
+export const food_list = [
+  {
+    _id: "1",
+    name: "Greek salad",
+    image: food_1,
+    price: 12,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: 2,
+    name: "Veg salad",
+    image: food_2,
+    price: 18,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "3",
+    name: "clover salad",
+    image: food_3,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "4",
+    name: "chicken salad",
+    image: food_4,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "salad",
+  },
+  {
+    _id: "5",
+    name: "Lasagna Rolls",
+    image: food_5,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    _id: "6",
+    name: "Peri Peri Rolls",
+    image: food_6,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    _id: "7",
+    name: "Chicken Rolls",
+    image: food_7,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    _id: "8",
+    name: "Veg Rolls",
+    image: food_8,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Rolls",
+  },
+  {
+    _id: "9",
+    name: "Ripple Ice Cream",
+    image: food_9,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Desert",
+  },
+  {
+    _id: "10",
+    name: "Fruit Ice Cream",
+    image: food_10,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts",
+  },
+  {
+    _id: "11",
+    name: "Jar Ice Cream",
+    image: food_11,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts",
+  },
+  {
+    _id: "12",
+    name: "Vanilla Ice Cream",
+    image: food_12,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Deserts",
+  },
+  {
+    _id: "13",
+    name: "Chicken Sandwich",
+    image: food_13,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    _id: "14",
+    name: "Vegan Sandwich",
+    image: food_14,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    _id: "15",
+    name: "Grilled Sandwich",
+    image: food_15,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    _id: "16",
+    name: "Bread Sandwich",
+    image: food_16,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Sandwich",
+  },
+  {
+    _id: "17",
+    name: "Cup Cake",
+    image: food_17,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake",
+  },
+  {
+    _id: "18",
+    name: "Vegan Cake",
+    image: food_18,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake",
+  },
+  {
+    _id: "19",
+    name: "Butterscotch Cake",
+    image: food_19,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake",
+  },
+  {
+    _id: "20",
+    name: "Sliced Cake",
+    image: food_20,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Cake",
+  },
+  {
+    _id: "21",
+    name: "ope",
+    image: food_21,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "22",
+    name: "ope",
+    image: food_22,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "23",
+    name: "ope",
+    image: food_23,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "24",
+    name: "ope",
+    image: food_24,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "25",
+    name: "ope",
+    image: food_25,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "26",
+    name: "ope",
+    image: food_26,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "27",
+    name: "ope",
+    image: food_27,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "28",
+    name: "ope",
+    image: food_28,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "29",
+    name: "ope",
+    image: food_29,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "30",
+    name: "ope",
+    image: food_30,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "31",
+    name: "ope",
+    image: food_31,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+  {
+    _id: "32",
+    name: "ope",
+    image: food_32,
+    price: 24,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "ope",
+  },
+];
